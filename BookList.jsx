c75bb671724985a07f0d528a29c398e03dca5146
Razerbeam115 <99705946+Razerbeam115@users.noreply.github.com>
2024-01-12 14:32:39 +0800

    Add files via upload

---
+++ BookList.jsx
@@ -0,0 +1,11 @@
+export default function BookList() {
+    return(
+        <div>
+            <h1>Book List</h1>
+        <li> Hunger Games</li>
+        <li> Twilight Saga</li>
+        <li> Divergent</li>
+        </div>
+    );
+
+    }
---
+++ MovieList.jsx
@@ -0,0 +1,11 @@
+export default function MovieList() {
+    return(
+        <div>
+            <h1>MovieList</h1>
+        <p> Avengers </p>
+        <p> Enteng Kabisote </p>
+        <p> Rewind </p>
+        </div>
+    );
+
+    }
